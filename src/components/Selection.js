ef63d00dd83fbac6ef0b21d1296d6b50cc049536
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-11-27 02:13:30 +0530

    some components created

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,10 @@
+import React from "react";
+import "../styles/Child.css";
 
+export default function Selection() {
+  return (
+    <div className="fix-box">
+      <h2 className="subheading"></h2>
+    </div>
+  );
+}
