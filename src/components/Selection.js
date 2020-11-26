ca2c6e7aad50aa7159dbe0563f2dfc0fb5c081a2
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-11-27 02:22:36 +0530

    Selection component made

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "../styles/Child.css";
 
-export default function Selection() {
+export default function Selection({applyColor,selectionStyle}) {
+
+
+
   return (
-    <div className="fix-box">
-      <h2 className="subheading"></h2>
+    <div className="fix-box" style={selectionStyle} onClick={applyColor}>
+      <h2 className="subheading">Selection</h2>
     </div>
   );
 }
